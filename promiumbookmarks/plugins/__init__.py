261a855d3e4200256c2a7941d4d2b8b808c59a24
Wes Turner <wes@wrd.nu>
2015-02-08 14:44:16 -0600

    BUG: promiumbookmarks.plugins.PromiumPlugin: accept a conf={} argument

--- promiumbookmarks/plugins/__init__.py
+++ promiumbookmarks/plugins/__init__.py
@@ -3,6 +3,11 @@
 from __future__ import print_function
 
 class PromiumPlugin(object):
+    def __init__(self, conf=None):
+        if conf is None:
+            conf = {}
+        self.conf = conf
+
     def process_bookmarks(self, bookmarks_obj):
         return bookmarks_obj
 
