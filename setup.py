b670e1c81f9c1d76ccc04e97f562c2d385c52dc1
Wes Turner <wes@wrd.nu>
2015-10-28 03:24:46 -0500

    BLD: setup.py, requirements.txt: add tornado as a dependency (for pbmweb)

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 requirements = [
-    'jinja2'
+    'jinja2',
+    'tornado'
 ]
 
 test_requirements = [
